843b2b491eab33efdd3429fcb97f6e674c5cfc89
blueriver97 <rladudwo0908@naver.com>
2025-12-20 04:30:04 +0900

    v0.0.48:  Polaris 및 Trino 설정 통합 및 보안 강화

---
+++ platform/polaris/init_key.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# secret 디렉토리가 없으면 생성, 있으면 무시
+mkdir -p secret
+
+# Polaris Token-broker key files
+openssl genrsa -out secret/private-key.pem 2048
+openssl rsa -in secret/private-key.pem -pubout -out secret/public-key.pem
+
+# Trino Login User/Password
+touch secret/password.db
+htpasswd -b -B -C 10 secret/password.db trino trinoadmin
+
+# Trino SSL key files
+openssl genrsa -out secret/server.key 2048
+openssl req -new -key secret/server.key -out secret/server.csr -config trino-config/san.cnf
+openssl x509 -req -days 3650 -in secret/server.csr -signkey secret/server.key -out secret/server.crt
+openssl pkcs12 -export -out keystore.p12 -inkey server.key -in server.crt -passout pass:password1! -name "localhost"
--- platform/polaris/rsa/generate_key.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-openssl genrsa -out private-key.pem 2048
-openssl rsa -in private-key.pem -pubout -out public-key.pem
