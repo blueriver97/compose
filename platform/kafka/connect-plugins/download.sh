9058209dac8da74682c7f58ed61f4c0bb374dbbf
blueriver97 <rladudwo0908@naver.com>
2026-01-10 00:51:32 +0900

    v0.0.71:  Kafka 플랫폼 설정 및 모니터링 기능 추가

---
+++ platform/kafka/connect-plugins/download.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# .env 파일 읽기
+while IFS='=' read -r key value; do
+    if [[ $key == "DEBEZIUM_VERSION" ]]; then
+        DEBEZIUM_VERSION=$value
+        echo "$key=$value"
+    elif [[ $key == "GROOVY_VERSION" ]]; then
+        GROOVY_VERSION=$value
+        echo "$key=$value"
+    fi
+done < ../.env
+
+# 2025-09-26 최종 확인. (Debezium=3.2.3, Groovy=4.0.28)
+declare -a jar_urls=(
+  "https://repo1.maven.org/maven2/io/debezium/debezium-connector-mysql/$DEBEZIUM_VERSION.Final/debezium-connector-mysql-$DEBEZIUM_VERSION.Final-plugin.tar.gz"
+  "https://repo1.maven.org/maven2/io/debezium/debezium-connector-sqlserver/$DEBEZIUM_VERSION.Final/debezium-connector-sqlserver-$DEBEZIUM_VERSION.Final-plugin.tar.gz"
+  "https://hub-downloads.confluent.io/api/plugins/confluentinc/kafka-connect-s3/versions/11.0.2/confluentinc-kafka-connect-s3-11.0.2.zip"
+  "https://repo1.maven.org/maven2/io/debezium/debezium-scripting/$DEBEZIUM_VERSION.Final/debezium-scripting-$DEBEZIUM_VERSION.Final.tar.gz"
+  "https://repo1.maven.org/maven2/org/apache/groovy/groovy/$GROOVY_VERSION/groovy-$GROOVY_VERSION.jar"
+  "https://repo1.maven.org/maven2/org/apache/groovy/groovy-jsr223/$GROOVY_VERSION/groovy-jsr223-$GROOVY_VERSION.jar"
+  "https://repo1.maven.org/maven2/org/apache/groovy/groovy-json/$GROOVY_VERSION/groovy-json-$GROOVY_VERSION.jar"
+)
+
+declare -a jars=()
+
+for url in "${jar_urls[@]}"; do
+    filename=$(basename "$url")
+    jars+=("$filename")
+done
+
+for (( i=0; i<${#jar_urls[@]}; i++ )); do
+    if [ -f "${jars[$i]}" ]; then
+        echo "${jars[$i]} ... existed"
+    else
+        wget "${jar_urls[$i]}" -O "${jars[$i]}"
+    fi
+done
---
+++ platform/kafka/connect-plugins/plugins_setup.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# .env 파일 읽기 (다운로드 스크립트와 동일한 변수 설정을 위해)
+echo "Reading .env file to determine file names..."
+while IFS='=' read -r key value; do
+    if [[ $key == "DEBEZIUM_VERSION" ]]; then
+        DEBEZIUM_VERSION=$value
+    elif [[ $key == "GROOVY_VERSION" ]]; then
+        GROOVY_VERSION=$value
+    fi
+done < ../.env
+
+# 변수가 올바르게 로드되었는지 확인
+if [ -z "$DEBEZIUM_VERSION" ] || [ -z "$GROOVY_VERSION" ]; then
+    echo "Error: DEBEZIUM_VERSION or GROOVY_VERSION could not be read from .env file."
+    exit 1
+fi
+
+echo "DEBEZIUM_VERSION=$DEBEZIUM_VERSION"
+echo "GROOVY_VERSION=$GROOVY_VERSION"
+echo ""
+
+# 대상 디렉터리 생성
+echo "Creating 'source' and 'sink' directories if they don't exist..."
+mkdir -p source sink
+
+# 다운로드된 파일 목록 정의
+# This list should match the files from your download script.
+declare -a downloaded_files=(
+  "debezium-connector-mysql-$DEBEZIUM_VERSION.Final-plugin.tar.gz"
+  "debezium-connector-sqlserver-$DEBEZIUM_VERSION.Final-plugin.tar.gz"
+  "confluentinc-kafka-connect-s3-11.0.2.zip"
+  "iceberg-iceberg-kafka-connect-1.9.2.zip"
+  "debezium-scripting-$DEBEZIUM_VERSION.Final.tar.gz"
+  "groovy-$GROOVY_VERSION.jar"
+  "groovy-jsr223-$GROOVY_VERSION.jar"
+  "groovy-json-$GROOVY_VERSION.jar"
+)
+
+# 각 파일을 적절한 디렉터리로 이동 또는 압축 해제
+echo "Starting to organize connector plugins and libraries..."
+for file in "${downloaded_files[@]}"; do
+    # 파일이 존재하는지 먼저 확인
+    if [ ! -f "$file" ]; then
+        echo "Warning: File '$file' not found. Skipping."
+        continue
+    fi
+
+    case "$file" in
+        *debezium-connector-mysql* | *debezium-connector-sqlserver*)
+            # Debezium Source Connectors and scripting library
+            echo "Extracting Source Plugin: $file -> to source/"
+            tar -xzf "$file" -C ./source/
+            ;;
+        *debezium-scripting*)
+            # Debezium scripting library
+            echo "Extracting Source Plugin: $file -> to source/"
+            tar -xzf "$file" -C ./source/
+            cp ./source/debezium-scripting/*.jar ./source/debezium-connector-mysql/
+            cp ./source/debezium-scripting/*.jar ./source/debezium-connector-sqlserver/
+
+            rm -rf ./source/debezium-scripting
+            ;;
+
+        *groovy*)
+            # Groovy jars are dependencies for Debezium scripting
+            echo "Moving Source Dependency: $file -> to source/"
+            cp "$file" ./source/debezium-connector-mysql/
+            cp "$file" ./source/debezium-connector-sqlserver/
+            ;;
+        *kafka-connect-s3* | *iceberg-kafka-connect*)
+            # Sink Connectors
+            echo "Extracting Sink Plugin: $file -> to sink/"
+            # Use -o to overwrite files without prompting
+            unzip -o "$file" -d ./sink/
+            ;;
+        *)
+            # Handle unknown files
+            echo "Warning: Don't know how to handle '$file'. Skipping."
+            ;;
+    esac
+done
+
+echo ""
+echo "File organization complete."
+echo "Please check the 'source' and 'sink' directories for the results."
