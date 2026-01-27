045672d5ad517e64d973c1be0a2ca989713e570f
blueriver97 <rladudwo0908@naver.com>
2026-01-27 23:50:28 +0900

    v0.0.87:  Iceberg 다운로드 URL을 GitHub Release로 변경

--- platform/kafka/connect-plugins/download.sh
+++ platform/kafka/connect-plugins/download.sh
@@ -15,9 +15,9 @@ while IFS='=' read -r key value; do
 done < ../.env
 
 # 2025-09-26 최종 확인. (Debezium=3.2.3, Groovy=4.0.28)
-# 2026-01-26 최종 확인. (Iceberg 1.10.1 추가)
+# 2026-01-26 최종 확인. (Iceberg 1.10.1 추가, Github Release 사용)
 declare -a jar_urls=(
-  "https://dlcdn.apache.org/iceberg/apache-iceberg-$ICEBERG_VERSION/apache-iceberg-$ICEBERG_VERSION.tar.gz"
+  "https://github.com/blueriver97/artifacts/releases/download/iceberg-kafka-connect-runtime/iceberg-kafka-connect-runtime-$ICEBERG_VERSION.zip"
   "https://repo1.maven.org/maven2/io/debezium/debezium-connector-mysql/$DEBEZIUM_VERSION.Final/debezium-connector-mysql-$DEBEZIUM_VERSION.Final-plugin.tar.gz"
   "https://repo1.maven.org/maven2/io/debezium/debezium-connector-sqlserver/$DEBEZIUM_VERSION.Final/debezium-connector-sqlserver-$DEBEZIUM_VERSION.Final-plugin.tar.gz"
   "https://hub-downloads.confluent.io/api/plugins/confluentinc/kafka-connect-s3/versions/11.0.2/confluentinc-kafka-connect-s3-11.0.2.zip"
