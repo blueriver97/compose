7239c002fe06872945460230a46ea187758128f7
blueriver97 <rladudwo0908@naver.com>
2026-01-31 01:21:27 +0900

    v0.0.89:  Iceberg 버전 1.10.0에서 1.10.1로 업데이트 및 디스크 구성 개선

--- platform/spark-cluster/download/download-pkg.sh
+++ platform/spark-cluster/download/download-pkg.sh
@@ -21,7 +21,7 @@ done < ../.env
 # 테스트 이력
 # 2025-09-23 최종 확인 (SPARK=3.5.6, HADOOP=3.3.4 수정 말고 그대로 사용할 것, 아래 패키지 외 필요 없음)
 # 2025-09-26 최종 확인 (SPARK-without-hadoop=3.5.6, HADOOP=3.3.6 적용 가능, Airflow 통해서 rds_to_iceberg 동작 확인)
-# 2025-12-15 최종 확인 ()
+# 2026-01-30 최종 확인 (SPARK=4.0.1, HADOOP=3.4.2-lean 적용 가능, bundle-2.29.52.jar 필요)
 
 declare -a package_urls=(
     "https://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}-lean.tar.gz"
