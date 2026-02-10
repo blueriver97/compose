845715d4d3526841fc191dbc6c4f2245c5de540d
blueriver97 <rladudwo0908@naver.com>
2026-02-11 00:46:57 +0900

    v0.1.21:  데이터베이스 호스트 변경 및 Polaris Credential Vending 관련 SSL 키 생성 스크립트 수정

--- platform/polaris/init_key.sh
+++ platform/polaris/init_key.sh
@@ -8,11 +8,11 @@ openssl genrsa -out secret/private-key.pem 2048
 openssl rsa -in secret/private-key.pem -pubout -out secret/public-key.pem
 
 # Trino Login User/Password
-touch secret/password.db
-htpasswd -b -B -C 10 secret/password.db trino trinoadmin
+# touch secret/password.db
+# htpasswd -b -B -C 10 secret/password.db trino trinoadmin
 
 # Trino SSL key files
-openssl genrsa -out secret/server.key 2048
-openssl req -new -key secret/server.key -out secret/server.csr -config trino-config/san.cnf
-openssl x509 -req -days 3650 -in secret/server.csr -signkey secret/server.key -out secret/server.crt
-openssl pkcs12 -export -out secret/keystore.p12 -inkey secret/server.key -in secret/server.crt -passout pass:password1! -name "localhost"
+# openssl genrsa -out secret/server.key 2048
+# openssl req -new -key secret/server.key -out secret/server.csr -config trino-config/san.cnf
+# openssl x509 -req -days 3650 -in secret/server.csr -signkey secret/server.key -out secret/server.crt
+# openssl pkcs12 -export -out secret/keystore.p12 -inkey secret/server.key -in secret/server.crt -passout pass:password1! -name "localhost"
